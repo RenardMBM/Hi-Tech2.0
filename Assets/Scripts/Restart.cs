ad6d4fcbb74f4384bbb28d8cdfec62b2e73fbb45
Vasili <vasil.ash@mail.ru>
2019-12-10 13:51:03 +1000

    added menu

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Button))]
+public class Restart : MonoBehaviour
+{
+
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
